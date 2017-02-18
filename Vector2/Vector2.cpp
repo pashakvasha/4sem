90ef14e04821f4676f2544965182430fde6f92c1
Pasha Kvasha <pashakvasha@yandex.ru>
2017-02-19 00:40:07 +0400

    Add files via upload
    Vector2

---
+++ Vector2/Vector2.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include "Vector2.h"
+#include <cmath>
+
+Vector2 Vector2::operator*(float k) const
+{
+	return Vector2(x * k, y * k);
+}
+
+Vector2 operator*(float k, const Vector2& v)
+{
+	return Vector2(k * v.x, k * v.y);
+}
+
+Vector2 Vector2::operator/(float k) const
+{
+	return Vector2(x / k, y / k);
+}
+
+Vector2 Vector2::operator-() const
+{
+	return Vector2(-x, -y);
+}
+
+Vector2  Vector2::operator+ (const Vector2& other) const
+{
+	return Vector2(x + other.x, y + other.y);
+}
+
+Vector2&  Vector2::operator += (const Vector2& other) {
+	x += other.x;
+	y += other.y;
+	return *this;
+}
+
+Vector2 Vector2::operator- (const Vector2& other) const
+{
+	return Vector2(x - other.x, y - other.y);
+}
+
+Vector2&  Vector2::operator -= (const Vector2& other) {
+	x -= other.x;
+	y -= other.y;
+	return *this;
+}
+
+float Vector2::operator^ (const Vector2& other) const
+{
+	return x * other.x - y * other.y;
+}
+
+float  Vector2::operator* (const Vector2& other) const
+{
+	return x * other.x + y * other.y;
+}
+
+float  Vector2::len() const
+{
+	return sqrt(x * x + y * y);
+}
+
+float  Vector2::squareLen() const
+{
+	return x * x + y * y;
+}
+
+Vector2 Vector2::perpendicular() const
+{
+	return Vector2(-y, x);
+}
+
+Vector2 Vector2::norm() const
+{
+	return Vector2( x / this->len() , y / this->len() );
+}
+
+Vector2& Vector2::rotate(float degree)
+{
+	float x_ = x, y_ = y;
+	x = x_ * cos(degree / 180 * PI) - y_ * sin(degree / 180 * PI);
+	y = y_ * cos(degree / 180 * PI) + x_ * sin(degree / 180 * PI);
+	return *this;
+}
+
+Vector2 Vector2::getRotated(float degree) const
+{
+	return Vector2(x * cos(degree / 180 * PI) - y * sin(degree / 180 * PI), y * cos(degree / 180 * PI) + x * sin(degree / 180 * PI));
+}
+
+std::ostream& operator <<(std::ostream& stream, const Vector2& v)
+{
+	stream << v.x << " " << v.y;
+	return stream;
+}
+
+std::istream& operator >> (std::istream& stream, Vector2& v)
+{
+	stream >> v.x >> v.y;
+	return stream;
+}
---
+++ Vector2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+#include "Vector2.h"
+
+int main() {
+	Vector2 a(1, 0), b(2, 3);
+	std::cout << a.getRotated(30);
+	std::cout << "\n";
+
+	return 0;
+}
